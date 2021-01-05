71ec23a1bf8dac377c7242b9caedfab30d905f31
Sebastian Nielsen <76970516+Fl1ck0ut@users.noreply.github.com>
2021-01-05 09:12:55 +0100

    Add files via upload

--- gps/__resource.lua
+++ gps/__resource.lua
@@ -1,10 +1,13 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
+	"lib/Proxy.lua",
+	"lib/Tunnel.lua",
 	"main.lua"
 }
 
 
 server_scripts {
+	"@vrp/lib/utils.lua",
 	"server.lua"
 }
--- gps/server.lua
+++ gps/server.lua
@@ -1,10 +1,20 @@
+--[[
+ _                             _                _  _     ____  _____   ____  _____ 
+| |                           | |             _| || |_  / ___||  _  | / ___||  _  |
+| |__    __ _  _ __ ___     __| |  ___  _ __ |_  __  _|/ /___ | |_| |/ /___ | |_| |
+| '_ \  / _` || '_ ` _ \   / _` | / _ \| '__| _| || |_ | ___ \\____ || ___ \\____ |
+| | | || (_| || | | | | | | (_| ||  __/| |   |_  __  _|| \_/ |.___/ /| \_/ |.___/ /
+|_| |_| \__,_||_| |_| |_|  \__,_| \___||_|     |_||_|  \_____/\____/ \_____/\____/ 
+                                                                                   
+                                                                                                                                                                                                                                                                                                                    
+]]
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
 
 vRPclient = Tunnel.getInterface("vRP", "vrp_firework") 
 vRP = Proxy.getInterface("vRP")
 
-vRP.defInventoryItem({'gps','GPS','Sætter GPS til Kokain mark', function(args)
+vRP.defInventoryItem({'gps','GPS','Sætter GPS til Kokain Mark', function(args)
     local choices = {}
     choices['> Brug'] = {function(player,choice,mod)
         local user_id = vRP.getUserId({player})
