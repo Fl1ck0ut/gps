9eb299238edef0c8d4c0ae92b8808779cf87f8af
Sebastian Nielsen <76970516+Fl1ck0ut@users.noreply.github.com>
2021-01-08 22:27:49 +0100

    Add files via upload

--- gps/main.lua
+++ gps/main.lua
@@ -8,14 +8,20 @@
                                                                                    
                                                                                                                                                                                                                                                                                                                     
 ]]
-local box = nil
-local animlib = 'anim@mp_fireworks'
 
+local player = GetPlayerPed()
 
-RegisterNetEvent('flicks-gps:start')
+RegisterNetEvent('flicks-gps:start') --finder event
 AddEventHandler('flicks-gps:start', function()
 
-	SetNewWaypoint(1216.4000244141+0.0001,-2947.0124511719+0.0001)
+	exports['progressBars']:startUI(4500, "Sætter GPS") --Progressbar
+	FreezeEntityPosition(player, true) --fryser spiller
+	TaskStartScenarioInPlace(PlayerPedId(), "WORLD_HUMAN_STAND_MOBILE", 0, true) --laver emote
+	Citizen.Wait(4500)
+	FreezeEntityPosition(player, false) --unfryser spiller
+	SetNewWaypoint(1216.4000244141+0.0001,-2947.0124511719+0.0001) --Sætter GPS
+	exports['mythic_notify']:DoCustomHudText('inform', 'Du satte GPS')
+	ClearPedTasks(player)
 
 end)
 
--- gps/server.lua
+++ gps/server.lua
@@ -11,18 +11,18 @@
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
 
-vRPclient = Tunnel.getInterface("vRP", "vrp_firework") 
+vRPclient = Tunnel.getInterface("vRP", "vrp_gps") 
 vRP = Proxy.getInterface("vRP")
 
-vRP.defInventoryItem({'gps','GPS','Sætter GPS til Kokain Mark', function(args)
+vRP.defInventoryItem({'gps','GPS','En anvanceret GPS', function(args)
     local choices = {}
-    choices['> Brug'] = {function(player,choice,mod)
+    choices['> Brug'] = {function(player,choice,mod) --Laver Valgmulighed
         local user_id = vRP.getUserId({player})
         if user_id ~= nil then
-			vRP.tryGetInventoryItem({user_id,"gps",1,true})
-			TriggerClientEvent('flicks-gps:start', player)
+			vRP.tryGetInventoryItem({user_id,"gps",1,true}) --tager item
+			TriggerClientEvent('flicks-gps:start', player) --Køre eventet
             vRP.closeMenu({player})
         end
     end}
     return choices
-end, 0.50})
+end, 0.50}) --vægt af item
